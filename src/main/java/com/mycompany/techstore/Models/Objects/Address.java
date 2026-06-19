477e2dab25ef6ca7dd98774e7fa0a5a2fb10f498
Vo Luu Tuong Anh <anhvlt.2006@outlook.com>
2026-06-19 23:53:07 +0700

    Update schema of object

--- src/main/java/com/mycompany/techstore/Models/Objects/Address.java
+++ src/main/java/com/mycompany/techstore/Models/Objects/Address.java
@@ -6,28 +6,24 @@ public class Address {
 
     private final int address_id;
     private final int user_id;
-    private final String line1;
-    private final String line2;
-    private final String city;
-    private final String state;
+    private final String receiver_name;
+    private final String phone;
+    private final String province;
     private final String postal_code;
-    private final String country;
+    private final String ward;
     private final boolean is_default;
     private final Timestamp created_at;
-    private final Timestamp updated_at;
 
-    public Address(int address_id, int user_id, String line1, String line2, String city, String state, String postal_code, String country, boolean is_default, Timestamp created_at, Timestamp updated_at) {
+    public Address(int address_id, int user_id, String receiver_name, String phone, String province, String postal_code, String ward, boolean is_default, Timestamp created_at) {
         this.address_id = address_id;
         this.user_id = user_id;
-        this.line1 = line1;
-        this.line2 = line2;
-        this.city = city;
-        this.state = state;
+        this.receiver_name = receiver_name;
+        this.phone = phone;
         this.postal_code = postal_code;
-        this.country = country;
+        this.province = province;
+        this.ward = ward;
         this.is_default = is_default;
         this.created_at = created_at;
-        this.updated_at = updated_at;
     }
 
     public int getAddress_id() {
@@ -38,28 +34,24 @@ public class Address {
         return user_id;
     }
 
-    public String getLine1() {
-        return line1;
+    public String getReceiver_name() {
+        return receiver_name;
     }
 
-    public String getLine2() {
-        return line2;
+    public String getPhone() {
+        return phone;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
+    public String getProvince() {
+        return province;
     }
 
     public String getPostal_code() {
         return postal_code;
     }
 
-    public String getCountry() {
-        return country;
+    public String getWard() {
+        return ward;
     }
 
     public boolean isIs_default() {
@@ -68,9 +60,5 @@ public class Address {
 
     public Timestamp getCreated_at() {
         return created_at;
-    }
-
-    public Timestamp getUpdated_at() {
-        return updated_at;
     }
 }
